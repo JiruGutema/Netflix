30f03f89a9602e1d528493ec349b17a89011cba1
JiruGutema <jethior1@gmail.com>
2024-12-03 01:47:30 +0300

    utils are added. axios and fetching data from THE MOVIE DB

---
+++ src/Utils/Requests.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const API = process.env.API;
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API}&with_networks-213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API}&with_genres-10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API}&with_genres=99`,
+  fetchTvShow: `tv/popular?api_key=${API}&language=en-US&page=1`,
+};
+export default requests;
